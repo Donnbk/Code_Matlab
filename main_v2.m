d9aa9860d225b3930e36eacba1013fc6195ac942
Donnbk <ngongocdobk@gmail.com>
2021-06-21 16:52:18 +0700

    Update main_v2.m file

--- main_v2.m
+++ main_v2.m
@@ -11,7 +11,7 @@ t_start = 0; t_end = 20; dt_iter = 0.1; t_iter = 0;
 x_start = -50; x_end = 50; dx_iter = 0.5; x_iter = 0;
 x_start_fig = x_start;  
 %%
-% Create vector for phi_x,f1,f2,f3
+% Create vector for x,f1, f2, f3, T_matrix
 n_x = round((x_end-x_start)/dx_iter) + 1; %number of frames
 x = zeros(1,n_x); f1 = zeros(1,n_x); f2 = zeros(1,n_x);f3 = zeros(1,n_x);
 m_t = round((t_end-t_iter)/dt_iter) + 1;
