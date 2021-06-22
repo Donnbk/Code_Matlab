93885fd5098c6ff191e023128e4944159d9f7a0b
Donnbk <ngongocdobk@gmail.com>
2021-06-22 16:51:13 +0700

    Update main_v3.m file

--- main_v3.m
+++ main_v3.m
@@ -12,7 +12,7 @@ w2 = vp2*2*pi()/lamda_2; k2 = 2*pi()/lamda_2; phi_02 = 0;
 t_start = 0; t_end = 20; dt_iter = 0.1; t_iter = 0;
 x_start = -50; x_end = 50; dx_iter = 0.5; x_iter = 0; 
 %%
-% Create vector for x,f1, f2, f3, T_matrix
+% Create vector for x, f1, f2, f3, T_matrix
 n_x = round((x_end-x_start)/dx_iter) + 1; %number of frames
 x = zeros(1,n_x); 
 f1 = zeros(1,n_x); f2 = zeros(1,n_x);f3 = zeros(1,n_x);
